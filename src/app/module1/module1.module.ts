afffe40fa9941cc5dedc7dff65b29493048d699a
Chloé <chl.leclerc@gmail.com>
2022-09-23 11:43:47 +0200

    part2 modules lazy loading

---
+++ src/app/module1/module1.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Module1RoutingModule } from './module1-routing.module';
+import { Component1Component } from './component1/component1.component';
+
+
+@NgModule({
+  declarations: [
+    Component1Component
+  ],
+  imports: [
+    CommonModule,
+    Module1RoutingModule
+  ]
+})
+export class Module1Module { }
